980a6b0d61ed206ed33f39477dc0e913dd6d673e
Cathrin Whitmer <catwhitmer@gmail.com>
2020-05-01 15:37:25 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # Add your routes here
   match '/auth/:provider/callback', to: 'sessions#create', via: [:get, :post]
   
-   root 'welcome#home'
+  root 'welcome#home'
   get '/auth/facebook/callback' => 'sessions#create'
 
 end
